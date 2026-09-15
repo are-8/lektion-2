e7b659b33bbe3fc5ca8d09fb2c335e034df4e13f
are-8 <Are.b.rosin@gmail.com>
2026-09-15 14:14:05 +0200

    lagt till fler filer samt gjort övningsuppg

---
+++ src/LogicalOperators.java
@@ -0,0 +1,12 @@
+public class LogicalOperators {
+    public static void main(String[] args) {
+        System.out.println("logiska operationer");
+
+        System.out.println(true);
+        System.out.println(!true);
+        System.out.println(!false);
+
+        System.out.println();
+        System.out.println(true && false);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,47 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello word");
+        int age = 30;
+        float height = 1.75f;
+        char initial = 'A';
+        String name = "Alice";
+        boolean isStudent = true;
+
+        System.out.println(((Object) age).getClass().getName());
+        System.out.println(((Object) height).getClass().getName());
+        System.out.println(((Object) initial).getClass().getName());
+        System.out.println(((Object) name).getClass().getName());
+        System.out.println(((Object) isStudent).getClass().getName());
+
+        System.out.println();
+
+        System.out.println( 10 + 20);
+        System.out.println(100 - 30);
+        System.out.println(5 * 7);
+        System.out.println(20/4);
+        System.out.println(10%3);
+
+        System.out.println();
+        int a = 15;
+        int b = 20;
+
+        System.out.println(a == b);
+        System.out.println(a > b);
+        System.out.println( a <= b);
+
+        System.out.println();
+
+        int x = 10;
+        int y = 5;
+        int z = 20;
+
+        System.out.println( x > y && z > y);
+        System.out.println( x > y || z < y);
+
+        System.out.println();
+
+        System.out.println((10 + 5) * 2 > 20);
+
+
     }
 }
